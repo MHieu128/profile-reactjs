a124d20285a68ec044242360f0d394e26b9b5310
Lương Minh Hiếu <minhhieudn98@gmail.com>
2020-08-21 19:22:41 +0700

    change greeting

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tui là Hiếu Lương nè! ^^
+          Tui là Hiếu Lương đây! ^^
         </p>
         <a
           className="App-link"
